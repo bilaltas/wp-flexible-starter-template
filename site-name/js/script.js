d5239a44652de490e19004a84c084f53e3d654a9
Bilal TAS <bilaltas@me.com>
2018-09-20 14:27:56 +0300

    Some default JS added

--- site-name/js/script.js
+++ site-name/js/script.js
@@ -1,5 +1,62 @@
 jQuery(document).ready(function($){
 
+	/*--*--*--*--*--*--*--*--*--*--*-*--*--*--*--*--*--*--*--*
+
+			* -------  Smooth scroll to hash ------- *
+
+	-*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*/
+
+	// Select all links with hashes
+	$('a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.click(function(event) {
+		// On-page links
+		if (
+		  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+		  &&
+		  location.hostname == this.hostname
+		) {
+		  // Figure out element to scroll to
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		  // Does a scroll target exist?
+		  if (target.length) {
+		    // Only prevent default if animation is actually gonna happen
+		    event.preventDefault();
+		    $('html, body').animate({
+		      scrollTop: target.offset().top
+		    }, 1000, function() {
+		      // Callback after animation
+		      // Must change focus!
+		      var $target = $(target);
+		      $target.focus();
+		      if ($target.is(":focus")) { // Checking if the target was focused
+		        return false;
+		      } else {
+		        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+		        $target.focus(); // Set focus again
+		      };
+		    });
+		  }
+		}
+	});
+
+
+
+	/*--*--*--*--*--*--*--*--*--*--*-*--*--*--*--*--*--*--*--*
+
+	    * -------  Prevent moving when clicking # ------- *
+
+	-*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*/
+
+	$('a[href="#"]').click(function(e) {
+
+		e.preventDefault();
+
+	});
+
 
 
 });
