2a585d7dc84bc6bce6e76884bbafa6f7813b995c
Bilal TAS <bilaltas@me.com>
2019-10-09 20:16:04 +0300

    Components updated

--- templates/home-page.php
+++ templates/home-page.php
@@ -10,19 +10,95 @@ get_header();
 		<div class="wrap container">
 			<div class="col">
 
+
 				<!-- Remove this sample content -->
-				<h1>Heading 1</h1>
-				<h2>Heading 2</h2>
-				<h3>Heading 3</h3>
-				<h4>Heading 4</h4>
-				<h5>Heading 5</h5>
-				<h6>Heading 6</h6>
-				<p><b>Paragraph:</b> <i>Lorem ipsum dolor</i>, <u>sit amet consectetur</u> adipisicing elit. <a href="#">Tempora perspiciatis</a> numquam magni minus libero eveniet sed incidunt sapiente suscipit itaque laudantium iure autem facilis, quidem iste hic eum consectetur in? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque voluptate placeat nemo, soluta incidunt id quaerat? Autem in id ab possimus veritatis aliquid inventore distinctio eius, eum nisi. Accusantium!</p>
+				<div class="wrap xl-gutter-40">
+					<div class="col xl-1-2">
 
 
-				<input type="text" placeholder="Text Input">
-				<a href="#" class="button">Button</a><br><br>
-				<textarea cols="30" rows="10" placeholder="Textarea"></textarea>
+						<h2>Colors</h2>
+
+						<span class="text-color-dark">Dark Color</span><br>
+						<span class="text-color-darker">Darker Color</span><br>
+						<span class="text-color-darkest">Darkest Color</span><br>
+						<span class="text-color-light">Light Color</span><br>
+						<span class="text-color-lighter">Lighter Color</span><br>
+						<span class="text-color-lightest" style="background-color: black;">Lightest Color</span><br>
+						<br><br>
+
+
+					</div>
+					<div class="col xl-1-2">
+
+
+						<h2>Fonts</h2>
+
+						<span class="font-primary">Primary Font</span><br>
+						<span class="font-secondary">Secondary Font</span><br>
+						<br><br>
+
+
+					</div>
+					<div class="col xl-1-2">
+
+
+						<h2>Text</h2>
+
+						<p><b>Paragraph:</b> <i>Lorem ipsum dolor</i>, <u>sit amet consectetur</u> adipisicing elit. <a href="#">Tempora perspiciatis</a> numquam magni minus libero eveniet sed incidunt sapiente suscipit itaque laudantium iure autem facilis, quidem iste hic eum consectetur in? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque voluptate placeat nemo, soluta incidunt id quaerat? Autem in id ab possimus veritatis aliquid inventore distinctio eius, eum nisi. Accusantium!</p>
+						<br><br>
+
+
+					</div>
+					<div class="col xl-1-2">
+
+
+						<h2>Titles</h2>
+
+						<h1>Heading 1</h1>
+						<h2>Heading 2</h2>
+						<h3>Heading 3</h3>
+						<h4>Heading 4</h4>
+						<h5>Heading 5</h5>
+						<h6>Heading 6</h6>
+						<br><br>
+
+
+					</div>
+					<div class="col xl-1-2">
+
+
+						<h2>Contact Form</h2>
+
+						<input class="full" type="text" placeholder="Your Name"><br>
+						<input class="full" type="email" placeholder="Your Email"><br>
+						<input class="full" type="tel" placeholder="Phone"><br>
+						<input class="full" type="text" placeholder="City"><br>
+						<textarea class="full" cols="30" rows="1" placeholder="Notes"></textarea><br>
+						<select class="full">
+							<option value="">How did you hear about us?</option>
+							<option value="">Option 1</option>
+							<option value="">Option 2</option>
+							<option value="">Option 3</option>
+						</select><br>
+						<a href="#" class="button">Submit</a>
+						<br><br>
+
+
+					</div>
+					<div class="col xl-1-2">
+
+
+						<h2>BUTTONS</h2>
+
+						<a href="#" class="button large">Large Button</a><br><br>
+						<a href="#" class="button">Multi Line<br>Button</a><br><br>
+						<a href="#" class="button">Standard Button</a><br><br>
+						<a href="#" class="button" disabled>Disabled Button</a><br><br>
+						<br><br>
+
+
+					</div>
+				</div>
 				<!-- Remove this sample content -->
 
 
