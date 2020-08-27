012bdd7714bb4806205f262f2f14f6bf2871b976
Bilal Tas <bilaltas@me.com>
2020-08-27 09:52:44 +0300

    Meta description removed by default

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=6.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<?php wp_head(); ?>
 
